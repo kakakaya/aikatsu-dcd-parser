2fd896b8794f66f811bccb3af5690bb27838c56f
kakakaya <kakakaya+git@gmail.com>
2018-12-02 13:58:23 +0900

    Add user command implement

---
+++ cmd/dcdkatsu/user.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kakakaya/aikatsu-dcd-parser"
+	"github.com/urfave/cli"
+)
+
+func user(c *cli.Context) error {
+	fmt.Printf("Hello %q", c.Args().Get(0))
+
+	fmt.Println("Hello friend!")
+
+	var idolID = c.Args().Get(0)
+	fmt.Println(idolID)
+	idol, err := dcdkatsu.FetchIdol(idolID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%+v\n", idol)
+
+	return nil
+}
