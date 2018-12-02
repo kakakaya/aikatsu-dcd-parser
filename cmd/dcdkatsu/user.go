3fb4856d7a829fbbbec7434d6652d53803ec20eb
kakakaya <kakakaya+git@gmail.com>
2018-12-02 14:23:24 +0900

    Delete unused output

--- cmd/dcdkatsu/user.go
+++ cmd/dcdkatsu/user.go
@@ -8,18 +8,14 @@ import (
 )
 
 func user(c *cli.Context) error {
-	fmt.Printf("Hello %q", c.Args().Get(0))
-
-	fmt.Println("Hello friend!")
-
 	var idolID = c.Args().Get(0)
-	fmt.Println(idolID)
 	idol, err := dcdkatsu.FetchIdol(idolID)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 	fmt.Printf("%+v\n", idol)
+	fmt.Println(idol.AvatarURL)
 
 	return nil
 }
