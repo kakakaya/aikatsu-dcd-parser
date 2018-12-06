8ee867cc53e10d63498b7b65473b2c72e262b08c
kakakaya <kakakaya+git@gmail.com>
2018-12-07 01:58:35 +0900

    Change const name

--- idol_test.go
+++ idol_test.go
@@ -4,10 +4,10 @@ import (
 	"testing"
 )
 
-const test_idol_id = "RXL7z1ut6wQ5sv3x"
+const testIdolD = "RXL7z1ut6wQ5sv3x"
 
 func TestFetchIdol(t *testing.T) {
-	idol, err := FetchIdol(test_idol_id)
+	idol, err := FetchIdol(testIdolD)
 	if err != nil {
 		t.Logf("%+v", idol)
 		t.Fatalf("Error occured while fetching idol.  %v.", err)
