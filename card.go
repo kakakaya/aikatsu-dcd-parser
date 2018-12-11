7ce484255d791c96127673e047a50c31130fe8a1
kakakaya <kakakaya+git@gmail.com>
2018-12-12 01:14:05 +0900

    Add Card struct

---
+++ card.go
@@ -0,0 +1,19 @@
+package dcdkatsu
+
+// Card represents a card.
+// Any card would be fetched with user id, so Card has user's ID as "OwnerID".
+type Card struct {
+	Code   string `json:"code"`
+	Rarity string `json:"rarity"`
+	Name   string `json:"name"`
+
+	Type        string `json:"type"`
+	Category    string `json:"category"`
+	Brand       string `json:"brand"`
+	DressAppeal string `json:"dress_appeal"`
+	AppealPoint int    `json:"appeal_point"`
+
+	Owner   Idol   `json:"-"`
+	OwnerID string `json:"owner_id"`
+	Owned   bool   `json:"owned"`
+}
