74a2ff2bd41e942c485de499279b03efce4e4e82
kakakaya <kakakaya+git@gmail.com>
2018-12-13 01:16:39 +0900

    Implementing cards parsing, not works well

--- digital_binder.go
+++ digital_binder.go
@@ -16,7 +16,7 @@ type DigitalBinder struct {
 	SeriesID   int    `json:"series_id"`
 	SeriesName string `json:"series_name"`
 
-	Cards []Card `json:"-"`
+	Cards []Card `json:"cards"`
 
 	// Owner   Idol   `json:"-"`
 	OwnerID string `json:"owner_id"`
@@ -29,6 +29,7 @@ type DigitalBinder struct {
 // This data will be fetched from http://mypage.aikatsu.com/mypages/digital_binders/<ownerID>/<seriesID> .
 func FetchDigitalBinder(ownerID string, seriesID int) (DigitalBinder, error) {
 	db := DigitalBinder{
+		OwnerID: ownerID,
 		SeriesID: seriesID,
 		URL:      fmt.Sprintf("http://mypage.aikatsu.com/mypages/digital_binders/%s/%d", ownerID, seriesID),
 	}
@@ -48,6 +49,8 @@ func FetchDigitalBinder(ownerID string, seriesID int) (DigitalBinder, error) {
 
 	db.SeriesName = strings.Trim(doc.Find("#container > article > div > section > div.l_box > div.m_tit > span").Text(), " \n")
 
+	doc.Find("#container > article > div > section > div.l_box > div.m_inner > ul.m_dress > li").Each(parseAndSetCard(&db))
+
 	// Set DataGetDate
 	re := regexp.MustCompile("データ取得日：(.*)$")
 	dgd := re.ReplaceAllString(
@@ -61,4 +64,32 @@ func FetchDigitalBinder(ownerID string, seriesID int) (DigitalBinder, error) {
 	}
 
 	return db, nil
+}
+
+// parseAndSetCard creats function that
+// parses card info from binder.
+func parseAndSetCard(db *DigitalBinder) func (i int, s *goquery.Selection)  {
+	return func (i int, s *goquery.Selection) {
+		card := Card {
+			OwnerID: db.OwnerID,
+			SeriesID: db.SeriesID,
+		}
+
+		re := regexp.MustCompile("'(.*)'")
+		path, _ := s.Find("a").Attr("onclick")
+		card.URL = re.ReplaceAllString(path, "http://mypage.aikatsu.com/$1")
+
+		s.Find("a > div.m_dress_card_img > img").Children().First()
+
+
+		// Set owned
+		if s.Find("a > div.m_dress_card_img > img.is_medal").Length() == 0 {
+			card.Owned = false
+		} else {
+			card.Owned = true
+		}
+
+
+		db.Cards = append(db.Cards, card)
+	}
 }
