44a916f0e36918f567099445e9cd9a3280545531
kakakaya <kakakaya+git@gmail.com>
2018-12-14 00:40:15 +0900

    Implement card

--- card.go
+++ card.go
@@ -2,15 +2,21 @@ package dcdkatsu
 
 import (
 	"fmt"
+	"net/http"
+	"strconv"
 	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 // Card represents a card.
 // Any card would be fetched with user id, so Card has user's ID as "OwnerID".
 type Card struct {
-	Code   string `json:"code"`
+	Code string `json:"code"`
+	Name string `json:"name"`
+
 	Rarity string `json:"rarity"`
-	Name   string `json:"name"`
+	Stars  string `json:"stars"`
 
 	Type        string `json:"type"`
 	Category    string `json:"category"`
@@ -29,13 +35,73 @@ type Card struct {
 	detailed bool // true after Card.Detail called and succeed.
 }
 
-// FetchCard returns parsed Idol data.
+// FetchCard returns parsed Idol data for given url.
 func FetchCard(url string) (Card, error) {
+	var err error
+
+	// trim trailing slash
+	if string(url[len(url)-1]) == "/" {
+		url = url[0 : len(url)-1]
+	}
+	fmt.Println(url)
 	card := Card{
 		URL: url,
 	}
 	p := strings.Split(url, "/")
-	card.OwnerID = p[len(p)-2]
+	card.OwnerID = p[len(p)-1]
+	card.SeriesID, err = strconv.Atoi(p[len(p)-2])
+	if err != nil {
+		return card, err
+	}
+
+	res, err := http.Get(card.URL)
+	if err != nil {
+		return card, err
+	}
+
+	defer res.Body.Close()
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return card, err
+	}
+
+	header := strings.Split(strings.Trim(doc.Find("body > div.l_dress-datail > div > div.m_dress-datail-box > h5").Text(), " \n"), " ")
+	if len(header) != 3 {
+		return card, fmt.Errorf("Card parsing error: %v", header)
+	}
+	fmt.Println(header)
+	card.Code = header[0]
+	card.Name = header[2]
+
+	card.Type = doc.Find("body > div.l_dress-datail > div > div.m_dress-datail-box > div > div.m_dataCol > dl.m_type > dd > img").AttrOr("alt", "")
+	card.Category = doc.Find("body > div.l_dress-datail > div > div.m_dress-datail-box > div > div.m_dataCol > dl.m_attribute > dd > span").Text()
+
+	_brand := doc.Find("body > div.l_dress-datail > div > div.m_dress-datail-box > div > div.m_dataCol > dl.m_brand > dd > span")
+	if _brand.Length() == 0 {
+		card.Brand = doc.Find("body > div.l_dress-datail > div > div.m_dress-datail-box > div > div.m_dataCol > dl.m_brand > dd > img").AttrOr("alt", "")
+	} else {
+		card.Brand = _brand.Text()
+	}
+
+	card.DressAppeal = doc.Find("body > div.l_dress-datail > div > div.m_dress-datail-box > div > div.m_dataCol > dl.m_appeal > dd > span").Text()
+	card.AppealPoint, err = strconv.Atoi(doc.Find("body > div.l_dress-datail > div > div.m_dress-datail-box > div > div.m_dataCol > dl.m_point > dd > span").Text())
+	if err != nil {
+		return card, err
+	}
+	// Set owned
+	if doc.Find("body > div.l_dress-datail > div > div.m_dress-datail-box > div > div.m_imgCol > div > img.is_medal").Length() == 0 {
+		card.Owned = false
+	} else {
+		card.Owned = true
+	}
+	card.ImageURL = fmt.Sprintf("http://mypage.aikatsu.com%s", doc.Find("body > div.l_dress-datail > div > div.m_dress-datail-box > div > div.m_imgCol > div > img").First().AttrOr("src", ""))
+
+	_rare := doc.Find("body > div.l_dress-datail > div > div.m_dress-datail-box > div > div.m_dataCol > dl.m_rare > dd > span").Text()
+	card.Stars = strings.Repeat("★", strings.Count(_rare, "★"))
+	card.Rarity = strings.Trim(_rare, "★")
+
 	return card, nil
 }
 
